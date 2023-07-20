a445b87e2b9379b9dc4c6814ebd65029a9ed1608
Shironim <dimasseto.id@gmail.com>
2023-07-20 17:34:32 +0700

    Task Module 2.3

---
+++ src/model/music.js
@@ -0,0 +1,34 @@
+let music = [
+  {
+    title : "Pijaraya",
+    artist : "Soegi Bornean",
+    url: "https://music.youtube.com/watch?v=dONTSATGKvI&feature=share",
+    played: 0
+  },
+  {
+    title : "Dan",
+    artist : "Sheila on 7",
+    url: "https://music.youtube.com/watch?v=k1BfsO0mxWQ&feature=share",
+    played: 0
+  },
+  {
+    title : "Kota",
+    artist : "Dere",
+    url: "https://music.youtube.com/watch?v=-yxJHtg_w0k&feature=share",
+    played: 0
+  },
+  {
+    title : "Taruh",
+    artist : "Nadin Amizah",
+    url: "https://music.youtube.com/watch?v=xQlS1a99vwk&feature=share",
+    played: 0
+  },
+  {
+    title : "Terpikat Senyummu",
+    artist : "Idgitaf",
+    url: "https://music.youtube.com/watch?v=0CgGmIUNQSI&feature=share",
+    played: 0
+  },
+];
+
+export default music;
